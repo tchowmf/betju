7c49a67fd458dd0649d04005ac551ac545f73c86
Vitor Ferreira <vitor.marostica@gmail.com>
2024-07-22 15:44:00 -0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +22,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS schema
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         View::composer('*', function ($view) {
             if (Auth::check()) {
+                /** @var \App\Models\User $user **/
                 $user = Auth::user();
                 $user->load('role');
                 $view->with('authUser', $user);
@@ -30,3 +37,4 @@ class AppServiceProvider extends ServiceProvider
         });
     }
 }
+
