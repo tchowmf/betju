fad974fed9818bc2075c7c418801ff5dacbadaf0
Vitor Ferreira <vitor.marostica@gmail.com>
2024-07-22 14:33:55 -0300

    session cookie = true

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Login - Bet Juuu - Login</title>
+    <title>Bet Juuu - Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
