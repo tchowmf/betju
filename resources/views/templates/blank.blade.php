a9e93e7ca50e4bbd2ffc9e20c08685183d311dd7
Vitor Ferreira <vitor.marostica@gmail.com>
2024-07-23 15:52:56 -0300

    updt: sidebar logo

--- resources/views/templates/blank.blade.php
+++ resources/views/templates/blank.blade.php
@@ -29,9 +29,9 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                    <img src="{{ asset('img/alias.png') }}" alt="Logo" style="width: 50px; height: 65px;">
                 </div>
                 <div class="sidebar-brand-text mx-3">Bet Juuu</div>
             </a>
