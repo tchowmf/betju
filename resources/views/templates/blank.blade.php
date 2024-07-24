cd3f8f3461004ec59c278c896e519938d2477cf2
Vitor Ferreira <vitor.marostica@gmail.com>
2024-07-24 14:05:56 -0300

    updt: logo height

--- resources/views/templates/blank.blade.php
+++ resources/views/templates/blank.blade.php
@@ -31,7 +31,7 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
                 <div class="sidebar-brand-icon">
-                    <img src="{{ asset('img/BETJUUU-OFICIAL.png') }}" alt="Logo" style="width: 50px; height: 65px;">
+                    <img src="{{ asset('img/BETJUUU-OFICIAL.png') }}" alt="Logo" style="width: 50px; height: 50px;">
                 </div>
                 <div class="sidebar-brand-text mx-3">Bet Juuu</div>
             </a>
